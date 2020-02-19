d1b05a3fc01eb26bde3eb4858e6666770db66c3d
Ibrahim Boughalem <ibra.boughalem1995@gmail.com>
2020-02-19 09:04:46 +0100

    added count and stats services

--- services/count.js
+++ services/count.js
@@ -1,16 +1,49 @@
 const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
-exports.count = (client, from, to, callback) => {
-    // TODO Compter le nombre d'anomalies entre deux dates
+exports.count = async (client, from, to, callback) => {
+    const res = await client.count({
+        index: indexName,
+        body: {
+            query: {
+                range: {
+                    "@timestamp": {
+                        format: "yyyy-MM-dd",
+                        gte: from,
+                        lte: to
+                    }
+                }
+            }
+        }
+    });
     callback({
-        count: 0
+        count: res.body.count
     })
 }
 
-exports.countAround = (client, lat, lon, radius, callback) => {
-    // TODO Compter le nombre d'anomalies autour d'un point géographique, dans un rayon donné
+exports.countAround = async (client, lat, lon, radius, callback) => {
+    const res = await client.count({
+        index: indexName,
+        body: {
+            query: {
+                bool: {
+                    must: {
+                        match_all: {}
+                    },
+                    filter: {
+                        geo_distance: {
+                            distance : radius,
+                            location: {
+                                lat, 
+                                lon
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
     callback({
-        count: 0
+        count: res.body.count
     })
 }
--- services/stats.js
+++ services/stats.js
@@ -1,22 +1,128 @@
 const config = require('config');
 const indexName = config.get('elasticsearch.index_name');
 
-exports.statsByArrondissement = (client, callback) => {
-    // TODO Compter le nombre d'anomalies par arondissement
-    callback([]);
+exports.statsByArrondissement = async (client, callback) => {
+    const response = await client.search({
+        size: 0,
+        index: indexName,
+        body: {
+            aggs: {
+                arrondissements: {
+                    terms: {
+                        field: "arrondissement.keyword",
+                        order: { _count: "desc" },
+                        size: 20
+                    }
+                }
+            }
+        }
+    });
+    const res = response.body.aggregations.arrondissements.buckets.map(element => {
+        return {
+            "arrondissement": element.key,
+            "count": element.doc_count
+        };
+    });
+    callback(res);
 }
 
-exports.statsByType = (client, callback) => {
-    // TODO Trouver le top 5 des types et sous types d'anomalies
-    callback([]);
+exports.statsByType = async (client, callback) => {
+    const response = await client.search({
+        size: 0,
+        index: indexName,
+        body: {
+            aggs: {
+                types: {
+                    terms: {
+                        field: "type.keyword",
+                        order: { _count: "desc" },
+                        size: 5
+                    },
+                    aggs: {
+                        sous_types: {
+                            terms: {
+                                field: "sous_type.keyword",
+                                order: { _count: "desc" },
+                                size: 5
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    const res = response.body.aggregations.types.buckets.map(element => {
+        return {
+            "types": element.key,
+            "count": element.doc_count,
+            "sous_types": element.sous_types.buckets.map(sous_type => {
+                return {
+                    "sous_type": sous_type.key,
+                    "count": sous_type.doc_count
+                }
+            })
+        };
+    });
+
+    callback(res);
 }
 
-exports.statsByMonth = (client, callback) => {
-    // TODO Trouver le top 10 des mois avec le plus d'anomalies
-    callback([]);
+exports.statsByMonth = async (client, callback) => {
+    const response = await client.search({
+        size: 0,
+        index: indexName,
+        body: {
+            aggs: {
+                datesdeclaration: {
+                    terms: {
+                        field: "date_declaration.keyword",
+                        order: { _count: "desc" },
+                        size: 10
+                    }
+                }
+            }
+        }
+    });
+    const res = response.body.aggregations.datesdeclaration.buckets.map(element => {
+        return {
+            "month": element.key,
+            "count": element.doc_count
+        };
+    });
+    callback(res);
 }
 
-exports.statsPropreteByArrondissement = (client, callback) => {
-    // TODO Trouver le top 3 des arrondissements avec le plus d'anomalies concernant la propreté
-    callback([]);
+exports.statsPropreteByArrondissement = async (client, callback) => {
+    const response = await client.search({
+        size: 0,
+        index: indexName,
+        body: {
+            query: {
+                bool: {
+                    must: {
+                        match: {
+                            type: "Propreté"
+                        }
+                    }
+                }
+            },
+            aggs: {
+                arrondissements: {
+                    terms: {
+                        field: "arrondissement.keyword",
+                        order: { _count: "desc" },
+                        size: 3
+                    }
+                }
+            }
+        }
+    });
+    const res = response.body.aggregations.arrondissements.buckets.map(element => {
+        return {
+            "arrondissement": element.key,
+            "count": element.doc_count
+        };
+    });
+    callback(res);
 }
